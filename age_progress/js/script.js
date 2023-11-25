e1eb826190e80b49dcf98f77e6a63751a5fdc9c5
egprogram <runrunrinmaru@gmail.com>
2023-11-26 00:42:28 +0900

    age-progress

---
+++ age_progress/js/script.js
@@ -0,0 +1,40 @@
+$(function(){
+  var birthDay = "1999-1-22";
+
+  var age = calcAge(birthDay);
+  var progress = calcProgress(birthDay);
+
+  $(".js-calc-age").html(age + "歳");
+  $(".js-calc-progress").css("width", progress + "%");
+});
+
+function calcAge(day){
+  var birthDate = new Date(day);
+
+  var currentDate = new Date();
+
+  var age = currentDate.getFullYear() - birthDate.getFullYear();
+
+  // 誕生日がまだ来ていない場合は、1歳引く
+  if (currentDate.getMonth() < birthDate.getMonth() ||
+     (currentDate.getMonth() === birthDate.getMonth() && currentDate.getDate() < birthDate.getDate())) {
+    age -= 1;
+  };
+
+  return age;
+};
+
+function calcProgress(day){
+  var days = day.split("-");
+  var month = days[1];
+  var day = days[2];
+
+  var currentDate = new Date();
+  var currentYear = currentDate.getFullYear();
+
+  var birthDate = new Date(currentYear, month, day);
+  var timeDiff = Math.abs(currentDate.getTime() - birthDate.getTime());
+  var daysDiff = Math.ceil(timeDiff / (1000 * 3600 * 24));
+
+  return Math.ceil((daysDiff / 365) * 100);
+};
