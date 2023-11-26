06ce7631acabe07a835ed08fdc669c5e75023b9a
egprogram <runrunrinmaru@gmail.com>
2023-11-26 22:21:44 +0900

    プログレスバーの計算を修正

--- age_progress/js/script.js
+++ age_progress/js/script.js
@@ -1,5 +1,5 @@
 $(function(){
-  var birthDay = "1999-1-22";
+  var birthDay = "1999/1/22"; // safari対策で "/" に
 
   var age = calcAge(birthDay);
   var progress = calcProgress(birthDay);
@@ -25,15 +25,25 @@ function calcAge(day){
 };
 
 function calcProgress(day){
-  var days = day.split("-");
+  var days = day.split("/");
   var month = days[1];
   var day = days[2];
 
+  // 現在の日時
   var currentDate = new Date();
   var currentYear = currentDate.getFullYear();
 
+  // 誕生日の日時
   var birthDate = new Date(currentYear, month, day);
+
   var timeDiff = Math.abs(currentDate.getTime() - birthDate.getTime());
+  if(currentDate.getTime() > birthDate.getTime()){
+    timeDiff = Math.abs(currentDate.getTime() - birthDate.getTime());
+  }else{
+    birthDate = new Date(currentYear - 1, month, day);
+    timeDiff = Math.abs(currentDate.getTime() - birthDate.getTime());
+  }
+  
   var daysDiff = Math.ceil(timeDiff / (1000 * 3600 * 24));
 
   return Math.ceil((daysDiff / 365) * 100);
